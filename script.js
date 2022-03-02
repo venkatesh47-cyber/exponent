8b63478497dee854699214ee63c26db600ef37fc
venkatesh s <79565162+venkatesh47-cyber@users.noreply.github.com>
2022-03-02 16:52:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var x=document.getElementById("demo1").value;
+    var y=document.getElementById("demo2").value;
+    var z;
+    z=x**y;
+    document.getElementById("demo3").innerHTML=z;
+}
